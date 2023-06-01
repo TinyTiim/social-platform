2165ae30f9c32e979588b75346e33a296c9bcdea
unknown <sophia.palomo12@gmail.com>
2023-06-01 14:50:45 -0700

    added users put and delete and tested on insomnia.

--- index.js
+++ index.js
@@ -42,6 +42,41 @@ app.post('/users', async (req, res) => {
   }
 });
 
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { username, email } = req.body;
+
+  try {
+    const updatedUser = await User.findByIdAndUpdate(id, { username, email }, { new: true });
+
+    if (!updatedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json(updatedUser);
+  } catch (error) {
+    console.error('Error updating user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deletedUser = await User.findByIdAndRemove(id);
+
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error('Error deleting user:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
