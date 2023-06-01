7dd3120f68da36a0b6c4b401d059058012806d34
unknown <sophia.palomo12@gmail.com>
2023-06-01 14:33:52 -0700

    added files reaction and thought js and schema

---
+++ models/reaction.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId()
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280
+  },
+  username: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: function() {
+      return this.createdAt.toISOString(); 
+    }
+  }
+});
+
+const Reaction = mongoose.model('Reaction', reactionSchema);
+
+module.exports = Reaction;
--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const usersSchema = new mongoose.Schema({
   username: {
